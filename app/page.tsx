8d037e33f687ae72ad9e4d6c1461ee44931af6a4
KonstantinVVictoria <konstantinvictoria@gmail.com>
2025-10-23 19:11:32 -0700

    fixed viewport size not adjusting to mobile address bar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Section name="splash-page" flow="column" height="92vh" centered>
+    <Section name="splash-page" flow="column" height="92dvh" centered>
       <Banner
         video_source="/vids/Xakari_Cinematic.mp4"
         width="100%"
