cd88d7b2b256234880602deb32e389406fb4c488
KonstantinVVictoria <konstantinvictoria@gmail.com>
2025-10-24 23:50:55 -0700

    added frontpage highlights

--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -4,14 +4,17 @@ interface JoinProps {}
 export default function Join(props: JoinProps) {
   return (
     <div className={styles.container}>
-      <img className={styles.icon} src="/imgs/xakari_icon.png"></img>
-      <h2 className={styles.title}>Join the Fitness Revolution</h2>
-      <p className={styles.desc}>
-        Start your fitness journey with a free AI workout app!
-      </p>
-      <button className={styles.button}>
-        <div className={styles.button_border}></div>Join Now.
-      </button>
+      <div>
+        <img className={styles.icon} src="/imgs/xakari_icon.png"></img>
+        <h2 className={styles.title}>Join the Fitness Revolution</h2>
+        <p className={styles.desc}>
+          Start your fitness journey with a free AI workout app!
+        </p>
+        <button className={styles.button}>
+          <div className={styles.button_border}></div>Join Now.
+        </button>
+      </div>
+      <div className={styles.highlights}></div>
     </div>
   );
 }
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,9 +3,14 @@ interface NavBarProps {}
 
 export default function NavBar(props: NavBarProps) {
   return (
-    <nav className={styles.container}>
-      <img className={styles.logo} src="/imgs/xakari_logo.png"></img>
-      <img className={styles.icon} src="/imgs/xakari_icon.png"></img>
-    </nav>
+    <>
+      <div className={styles.container}>
+        <nav>
+          <img className={styles.logo} src="/imgs/xakari_logo.png"></img>
+          <img className={styles.icon} src="/imgs/xakari_icon.png"></img>
+        </nav>
+        <div className={styles.highlights}></div>
+      </div>
+    </>
   );
 }
